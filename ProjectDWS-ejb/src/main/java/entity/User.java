032e939c50b62543bd44e71bbac4d2cb26f8da76
alumno <alumno@172.16.205.110>
2016-11-11 11:34:20 +0100

    Commit User and index

--- ProjectDWS-ejb/src/main/java/entity/User.java
+++ ProjectDWS-ejb/src/main/java/entity/User.java
@@ -5,10 +5,114 @@
  */
 package entity;
 
+import java.io.Serializable;
+
 /**
  *
  * @author alumno
  */
-public class User {
-    
+public class User implements Serializable {
+    private int id;
+    private String user;
+    private String password;
+    private String name;
+    private String firstLastname;
+    private String secondLastname;
+    private int age;
+    private String addres;
+    private String email;
+    private int phone;
+
+    public User(int id, String user, String password, String name, String firstLastname, String secondLastname, int age, String addres, String email, int phone) {
+        this.id = id;
+        this.user = user;
+        this.password = password;
+        this.name = name;
+        this.firstLastname = firstLastname;
+        this.secondLastname = secondLastname;
+        this.age = age;
+        this.addres = addres;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstLastname() {
+        return firstLastname;
+    }
+
+    public void setFirstLastname(String firstLastname) {
+        this.firstLastname = firstLastname;
+    }
+
+    public String getSecondLastname() {
+        return secondLastname;
+    }
+
+    public void setSecondLastname(String secondLastname) {
+        this.secondLastname = secondLastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
 }
