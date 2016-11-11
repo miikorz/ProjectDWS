14510bb56b5a0caa2c6d85634e8e01a6f513ff18
alumno <miikorz@gmail.com>
2016-11-11 10:34:21 +0100

    Logic on Product object

--- ProjectDWS-ejb/src/main/java/entity/Product.java
+++ ProjectDWS-ejb/src/main/java/entity/Product.java
@@ -5,10 +5,72 @@
  */
 package entity;
 
-/**
- *
- * @author alumno
- */
-public class Product {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class Product implements Serializable {
+    private int productID, stock;
+    private String name, description;
+    private double price;
+    private ArrayList<String> extras;
+
+    public Product(int productID, int stock, String name, String description, double price, ArrayList<String> extras) {
+        this.productID = productID;
+        this.stock = stock;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.extras = extras;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public ArrayList<String> getExtras() {
+        return extras;
+    }
+
+    public void setExtras(ArrayList<String> extras) {
+        this.extras = extras;
+    }
+    
     
 }
