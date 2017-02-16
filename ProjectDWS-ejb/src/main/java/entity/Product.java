77d777b4e271158b2bffac8bb66125aab2799586
alumno <miikorz@gmail.com>
2017-02-16 13:40:52 +0100

    Producs to JPA working

--- ProjectDWS-ejb/src/main/java/entity/Product.java
+++ ProjectDWS-ejb/src/main/java/entity/Product.java
@@ -6,7 +6,6 @@
 package entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
